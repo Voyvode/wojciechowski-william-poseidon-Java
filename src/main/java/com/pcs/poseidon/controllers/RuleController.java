776bb603c601a5e6d080260a297bf3217e2c787e
Voyvode <voyvode@yandex.com>
2024-12-31 02:07:34 +0100

    Implement the RESTful API for Rule entity

--- src/main/java/com/pcs/poseidon/controllers/RuleController.java
+++ src/main/java/com/pcs/poseidon/controllers/RuleController.java
@@ -1,6 +1,8 @@
 package com.pcs.poseidon.controllers;
 
 import com.pcs.poseidon.domain.Rule;
+import com.pcs.poseidon.repositories.RuleRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,44 +14,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 
+/**
+ * The CRUD controller handling HTTP requests for Rule data management.
+ */
 @Controller
+@RequiredArgsConstructor
 @Slf4j
 public class RuleController {
-    // TODO: Inject RuleName service
+
+    private final RuleRepository ruleRepository;
 
     @RequestMapping("/rules/list")
     public String home(Model model) {
-        // TODO: find all RuleName, add to model
+        model.addAttribute("rules", ruleRepository.findAll());
         return "rules/list";
     }
 
     @GetMapping("/rules/add")
-    public String addRuleForm(Rule bid) {
+    public String addRuleForm(Rule rule) {
         return "rules/add";
     }
 
     @PostMapping("/rules/validate")
     public String validate(@Valid Rule rule, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
+        if (!result.hasErrors()) {
+            ruleRepository.save(rule);
+            model.addAttribute("rules", ruleRepository.findAll());
+            return "redirect:/rules/list";
+        }
         return "rules/add";
     }
 
     @GetMapping("/rules/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        var rule = ruleRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("Invalid rule id:" + id));
+        model.addAttribute("rule", rule);
         return "rules/update";
     }
 
     @PostMapping("/rules/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid Rule rule,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+    public String update(@PathVariable("id") Long id, @Valid Rule rule,
+                         BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "rules/update";
+        }
+        ruleRepository.save(rule);
+        model.addAttribute("rules", ruleRepository.findAll());
         return "redirect:/rules/list";
     }
 
     @GetMapping("/rules/delete/{id}")
-    public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+    public String delete(@PathVariable("id") Long id, Model model) {
+        var rule = ruleRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("Invalid rule id:" + id));
+        ruleRepository.delete(rule);
+        model.addAttribute("rules", ruleRepository.findAll());
         return "redirect:/rules/list";
     }
 
--- src/main/java/com/pcs/poseidon/domain/Rule.java
+++ src/main/java/com/pcs/poseidon/domain/Rule.java
@@ -1,22 +1,45 @@
 package com.pcs.poseidon.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+/**
+ * Rules are preprogrammed orders.
+ *
+ * Fields:
+ * - id: The unique identifier of the rule.
+ * - name: The name of the rule.
+ * - description: A description of the rule.
+ * - json: A JSON formatted string containing additional data about the rule.
+ * - template: The template associated with the rule.
+ * - sqlStr: The SQL string used by the rule.
+ * - sqlPart: A SQL fragment used as part of the rule.
+ */
 @Entity
 @Table(name = "rules")
 @Data
 public class Rule {
 
 	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
 	Long id;
+
+	@NotBlank(message = "Name is mandatory")
 	String name;
+
 	String description;
+
 	String json;
+
 	String template;
+
 	String sqlStr;
+
 	String sqlPart;
 
 }
