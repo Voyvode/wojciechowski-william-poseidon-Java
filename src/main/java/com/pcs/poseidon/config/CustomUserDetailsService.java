45725934af71c06bdc03a8fb78edbb550e76fc60
Voyvode <voyvode@yandex.com>
2025-01-08 14:20:58 +0100

    Configure authentication with DB users and roles

---
+++ src/main/java/com/pcs/poseidon/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.pcs.poseidon.config;
+
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+import com.pcs.poseidon.repositories.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = userRepository.findByUsername(username);
+		return new User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user.getRole()));
+	}
+
+	private List<GrantedAuthority> getGrantedAuthorities(String role) {
+		return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+	}
+
+}
--- src/main/java/com/pcs/poseidon/repositories/UserRepository.java
+++ src/main/java/com/pcs/poseidon/repositories/UserRepository.java
@@ -3,11 +3,11 @@ package com.pcs.poseidon.repositories;
 import com.pcs.poseidon.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+	User findByUsername(String username);
 
 }
